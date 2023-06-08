ef026be870907f0f91921d854bf12f85f49dc43e
Michael O'Grady <michael.ogrady@genesys.com>
2023-06-08 17:13:54 +0100

    initial commit

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
-const express = require('express')
+const express = require('express');
 
-const app = express()
-const PORT = 4000
+const app = express();
+const PORT = 4000;
 
 app.listen(PORT, () => {
-  console.log(`API listening on PORT ${PORT} `)
-})
+  console.log(`API listening on PORT ${PORT} `);
+});
 
 app.get('/', (req, res) => {
-  res.send('Hey this is my API running 🥳')
-})
+  res.json({
+    message: 'Hello, World!',
+  });
+});
 
 app.get('/about', (req, res) => {
-  res.send('This is my about route..... ')
-})
+  res.send('This is my about route..... ');
+});
 
 // Export the Express API
-module.exports = app
+module.exports = app;
